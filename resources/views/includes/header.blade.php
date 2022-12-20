576099052251b41b74596d6e28d76998ea7cbf67
rpcmoreira <rpcardoso2@outlook.com>
2022-12-20 12:35:45 +0000

    header menus mudados

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
   <a class="navbar-brand" href="{{ url('/') }}">Sell.Me</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"><span
-      class="navbar-toggler-icon"></span></button>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"><span class="navbar-toggler-icon"></span></button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
@@ -16,20 +15,32 @@
         <button class="btn btn-light my-sm-0" type="submit">Search</button>
       </form>
 
+      @guest
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-          Account
-        </a>
+      <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">Account</a>
         <ul class="dropdown-menu dropdown-menu-right">
-          
-          <li><a class="dropdown-item" href="{{ url('/login') }}">
-              {{ __('Login') }}</a></li>
-          <li><a class="dropdown-item" href="{{ url('/register') }}">
-              {{ __('Register') }}</a></li>
+        @if (Route::has('login'))
+        <a class="dropdown-item" href="{{ route('login') }}">{{ __('Login') }}</a>
+        @endif
+        @if (Route::has('register'))
+        <a class="dropdown-item" href="{{ route('register') }}">
+          {{ __('Register') }}
+        </a>
         </ul>
       </li>
+      @endif
+      @else
+      <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">{{ Auth::user()->name }}</a>  
+      <ul class="dropdown-menu dropdown-menu-right">
+        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+          @csrf
+        </form>
+      </ul>
+      </li>
+      @endguest
     </ul>
   </div>
-
 
 </nav>
