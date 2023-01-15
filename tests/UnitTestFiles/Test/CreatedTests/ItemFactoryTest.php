091b1fa7702f0f1c82136cf72e2a73aebc6af6c6
rpcmoreira <rpcardoso2@outlook.com>
2023-01-15 14:18:23 +0000

    Update ItemFactoryTest.php

--- tests/UnitTestFiles/Test/CreatedTests/ItemFactoryTest.php
+++ tests/UnitTestFiles/Test/CreatedTests/ItemFactoryTest.php
@@ -3,26 +3,27 @@
 use \App\Models\Item;
 use PHPUnit\Framework\TestCase;
 
-final class ItemFactoryTest extends TestCase{
+final class ItemFactoryTest extends TestCase
+{
 
-    public function testClassConstructor(){
+    public function testClassConstructor()
+    {
 
-    $item = new Item();
+        $item = new Item();
 
 
-    $this->assertEmpty($item->getName());
-    $this->assertEmpty($item->getCategory());
-    $this->assertSame(0.00, $item->getPrice());
-    
-    $item->setName('Drill Bit');
-    $item->setCategory('Others');
-    $item->setPrice(54.10);
+        $this->assertEmpty($item->getName());
+        $this->assertEmpty($item->getCategory());
+        $this->assertSame(0.00, $item->getPrice());
 
-    $this->assertSame('Drill Bit', $item->getName());
-    $this->assertSame("Others", $item->getCategory());
-    $this->assertEmpty($item->getDescription());
-    $this->assertSame(54.10, $item->getPrice());
+        $item->setName('Drill Bit');
+        $item->setCategory('Others');
+        $item->setPrice(54.10);
 
-}
-// Os testes serão colocados aqui
+        $this->assertSame('Drill Bit', $item->getName());
+        $this->assertSame("Others", $item->getCategory());
+        $this->assertEmpty($item->getDescription());
+        $this->assertSame(54.10, $item->getPrice());
+    }
+    // Os testes serão colocados aqui
 }
