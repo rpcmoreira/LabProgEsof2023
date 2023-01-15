b8eca9b08ae4b7a1f7ad4551a7e401efd5786bda
rpcmoreira <rpcardoso2@outlook.com>
2023-01-15 14:15:41 +0000

    Teste Item Alterado

--- tests/UnitTestFiles/Test/CreatedTests/ItemFactoryTest.php
+++ tests/UnitTestFiles/Test/CreatedTests/ItemFactoryTest.php
@@ -2,17 +2,26 @@
 
 use \App\Models\Item;
 use PHPUnit\Framework\TestCase;
+
 final class ItemFactoryTest extends TestCase{
+
     public function testClassConstructor(){
-    $item = Item::create([
-        'name' => 'Drill Bit',
-        'category' => 'Others',
-        'price' => 54.52,
-    ]);
-    $this->assertSame('Drill Bit', $item->name);
-    $this->assertSame("Others", $item->category);
-    $this->assertEmpty($item->description);
-    $this->assertSame(54.10, $item->price);
+
+    $item = new Item();
+
+
+    $this->assertEmpty($item->getName());
+    $this->assertEmpty($item->getCategory());
+    $this->assertSame(0.00, $item->getPrice());
+    
+    $item->setName('Drill Bit');
+    $item->setCategory('Others');
+    $item->setPrice(54.10);
+
+    $this->assertSame('Drill Bit', $item->getName());
+    $this->assertSame("Others", $item->getCategory());
+    $this->assertEmpty($item->getDescription());
+    $this->assertSame(54.10, $item->getPrice());
 
 }
 // Os testes serão colocados aqui
